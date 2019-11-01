f283ce4504f5409a67c893692bcaec86f6af2494
Mariia Shpura <mshpu@macmini095.softservecom.com>
2019-11-01 14:32:09 +0200

    Add error test for chat component and fix bugs

--- src/app/chat/chat.component.spec.ts
+++ src/app/chat/chat.component.spec.ts
@@ -8,29 +8,42 @@ import { MockStore } from '@ngrx/store/testing';
 import * as fromApp from '../store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { chatReducer, ChatState } from './store/chat.reducer';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { EffectsModule } from '@ngrx/effects';
 import { ChatEffects } from './store/chat.effects';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ChatService } from '../services/chat.service';
 import { Message } from '../models/message.interface';
 
+
+const MOCKED_MESSAGE_1: Message = {
+  id: 1,
+  message: 'yeah',
+  createdAt: new Date(),
+};
+const MOCKED_MESSAGE_2: Message = {
+  id: 2,
+  message: 'some text here',
+  createdAt: new Date(),
+};
+
 const mockedInitialState: ChatState = {
   ...chatReducer,
-  messages: [{
-    id: 1,
-    message: '',
-    createdAt: new Date(),
-  }]
+  messages: [
+    MOCKED_MESSAGE_1
+  ]
 };
+
+const RECEIVED_MESSAGES: Message[] = [
+  MOCKED_MESSAGE_1,
+  MOCKED_MESSAGE_2
+];
 
 const mockedReducer = (initialState = mockedInitialState, action) => {
   return chatReducer(initialState, action);
 };
 
 const spyChatService = jasmine.createSpyObj('ChatService', ['getMessages']);
-const mockedMessages: Message[] = [{id: 1, message: '', createdAt: new Date()}];
-
 
 describe('ChatComponent', () => {
   let component: ChatComponent;
@@ -60,8 +73,8 @@ describe('ChatComponent', () => {
     component = fixture.componentInstance;
     store = TestBed.get(Store);
     spyOn(store, 'dispatch').and.callThrough();
+
     MockedChatService = TestBed.get(ChatService);
-    MockedChatService.getMessages.and.returnValue(of(mockedMessages));
   });
 
   it('should create chat component', () => {
@@ -92,20 +105,19 @@ describe('ChatComponent', () => {
     expect(store.dispatch).toHaveBeenCalledWith(new LoadMessagesBegin());
   });
 
-  it('should set value for message from service', () => {
+  it('should receive messages list from store (mocked in service used by effects)', () => {
+    MockedChatService.getMessages.and.returnValue(of(RECEIVED_MESSAGES));
     component.ngOnInit();
-    store.dispatch(new LoadMessagesBegin());
     component.messages$.subscribe(res => {
-      expect(res).toEqual(mockedMessages);
+      expect(res).toEqual(RECEIVED_MESSAGES);
     });
   });
 
-  it('should call getMessages function when OnInit', () => {
+  it('should show previously received messages in case of error', () => {
+    MockedChatService.getMessages.and.returnValue(throwError(new Error('Some error')));
     component.ngOnInit();
-    expect(MockedChatService.getMessages).toHaveBeenCalled();
-  });
-
-  it('getMessages should return messages', () => {
-    MockedChatService.getMessages().subscribe(res => expect(res).toBe(mockedMessages));
+    component.messages$.subscribe(res => {
+      expect(res).toEqual([MOCKED_MESSAGE_1]);
+    });
   });
 });
