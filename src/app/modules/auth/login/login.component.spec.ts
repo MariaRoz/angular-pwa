3adb20033bf1da51168d2ab63bf21bcd8149d263
Mariia Shpura <mshpu@macmini095.softservecom.com>
2019-11-15 18:42:54 +0200

    init commit

--- src/app/modules/auth/login/login.component.spec.ts
+++ src/app/modules/auth/login/login.component.spec.ts
@@ -1,25 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {MaterialModule} from '../../../material.module';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms';
+import {Store, StoreModule} from '@ngrx/store';
+import {authReducer} from '../store/auth.reducer';
+import {EffectsModule} from '@ngrx/effects';
+import {AuthEffects} from '../store/auth.effect';
+import {HttpClientModule} from '@angular/common/http';
+import {AuthService} from '../auth.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MockStore} from '@ngrx/store/testing';
+import * as fromApp from '../../../store';
+import { LoginStart} from '../store/auth.action';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  }));
+  let store: MockStore<fromApp.AppState>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [MaterialModule, RouterTestingModule, FormsModule, StoreModule.forRoot({}), StoreModule.forFeature('auth', authReducer),
+        EffectsModule.forRoot([AuthEffects]), HttpClientModule, BrowserAnimationsModule],
+      declarations: [LoginComponent],
+      providers: [AuthService]
+    }).compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
   });
 
-  it('should create', () => {
+  it('should create the login component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call LoginStart action onclick onSubmit', () => {
+    // const email = fixture.debugElement.nativeElement.querySelector('#email')
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    expect(store.dispatch).toHaveBeenCalledWith(new LoginStart({email: undefined, password: undefined}));
+    });
 });
--- src/app/modules/auth/register/register.component.spec.ts
+++ src/app/modules/auth/register/register.component.spec.ts
@@ -1,25 +1,45 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import {MaterialModule} from '../../../material.module';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms';
+import {Store, StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {AuthEffects} from '../store/auth.effect';
+import {HttpClientModule} from '@angular/common/http';
+import {AuthService} from '../auth.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SingUpStart} from '../store/auth.action';
+import {MockStore} from '@ngrx/store/testing';
+import * as fromApp from '../../../store';
+import {authReducer} from '../store/auth.reducer';
 
-describe('AuthFormComponent', () => {
+describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
-  }));
+  let store: MockStore<fromApp.AppState>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [MaterialModule, RouterTestingModule, FormsModule, StoreModule.forRoot({}), StoreModule.forFeature('auth', authReducer),
+        EffectsModule.forRoot([AuthEffects]), HttpClientModule, BrowserAnimationsModule],
+      declarations: [RegisterComponent],
+      providers: [AuthService]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
   });
 
-  it('should create', () => {
+  it('should create the register component', () => {
     expect(component).toBeTruthy();
+  });
+  it('should call SingUpStart action onclick onSubmit', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    expect(store.dispatch).toHaveBeenCalledWith(new SingUpStart({email: undefined, password: undefined}));
   });
 });
