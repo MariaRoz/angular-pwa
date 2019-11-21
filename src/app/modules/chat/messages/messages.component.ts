ae485c1963b5adb2d53dff06679fff74262c5f17
Mariia Shpura <mshpu@macmini035.softservecom.com>
2019-11-21 17:25:27 +0200

    Fix layout for chat page

--- src/app/modules/chat/messages/messages.component.ts
+++ src/app/modules/chat/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Message } from '../../../models/message.interface';
 import {select, Store} from '@ngrx/store';
 import * as fromApp from '../../../store';
@@ -6,12 +6,14 @@ import { LoadMessagesBegin, StartSendingMessage } from '../store/chat.actions';
 import { Observable, of } from 'rxjs';
 import { selectMessages } from '../store/chat.selectors';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent implements OnInit, AfterViewChecked {
+  @ViewChild('scroll', {static: false}) private myScrollContainer: ElementRef;
   isLoading = false;
 
   public messages$: Observable<Message[]> = of([]);
@@ -19,6 +21,8 @@ export class MessagesComponent implements OnInit {
   constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit(): void {
+    this.scrollToBottom();
+
     this.messages$ = this.store.pipe(
       select(selectMessages)
     );
@@ -26,7 +30,17 @@ export class MessagesComponent implements OnInit {
     this.store.dispatch(new LoadMessagesBegin());
   }
 
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
   addMessage(message): void {
     this.store.dispatch(new StartSendingMessage({message}));
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
   }
 }
